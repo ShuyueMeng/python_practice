102ef0ce1fd0cdad297c26f60876de0168910d99
mengshuyue00 <mengshuyue00@163.com>
2026-08-08 05:14:44 +0800

    #

--- Week1_BMI_Calculator.py
+++ Week1_BMI_Calculator.py
@@ -1,8 +1,12 @@
-def main(height,weight):
+def bmi_cal(height,weight):
     return weight / height ** 2
+
+def main():
+    height = float(input("Please enter your height(m):"))
+    weight = float(input("Please enter your weight(kg):"))
+    print(f'Your BMI is: {bmi_cal(height, weight)}')
+
 
 
 if __name__ == "__main__":
-    height=float(input("Please enter your height(m):"))
-    weight = float(input("Please enter your weight(kg):"))
-    print(f'Your BMI is: {main(height,weight)}')
+    main()
